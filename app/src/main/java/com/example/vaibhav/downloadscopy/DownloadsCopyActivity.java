25734cb13be725be946b2bb4c8c1e0f46d8b643a
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-10 10:51:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/downloadscopy/DownloadsCopyActivity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.downloadscopy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DownloadsCopyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_downloads_copy);
+    }
+}
